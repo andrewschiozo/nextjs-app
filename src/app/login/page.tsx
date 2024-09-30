5957c5a91633dd76e9801731a5459eeb2c75ec97
Andrews <andrewschiozo@gmail.com>
2024-09-30 09:46:48 -0300

    feat: rota para formulário de login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormLogin from './components/FormLogin'
+
+const Page = async () => {
+  return (
+      <div>
+        <FormLogin />
+      </div>
+  )
+}
+
+export default Page
